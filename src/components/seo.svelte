9fff0da7785b1f8ad526d07897b836fd5a46ebd0
DoceAzedo <oi@lucasfernandes.com.br>
2021-10-03 17:27:26 -0300

    🔍🐛 Fix SEO meta tags

--- src/components/seo.svelte
+++ src/components/seo.svelte
@@ -6,11 +6,11 @@
 <meta name="title" content="{title}">
 <meta name="description" content="I am a fullstack developer, specially for the web, building stylish and responsive layouts and designing well structured APIs and databases.">
 
-<meta property="og:type" content="website">
-<meta property="og:url" content="https://lucasfernandes.com.br{path}">
-<meta property="og:title" content="{title}">
-<meta property="og:description" content="I am a fullstack developer, specially for the web, building stylish and responsive layouts and designing well structured APIs and databases.">
-<meta property="og:image" name="image" content="https://lucasfernandes.com.br/thumbnail.jpg">
+<meta prefix="og: http://ogp.me/ns#" property="og:type" content="website">
+<meta prefix="og: http://ogp.me/ns#" property="og:url" content="https://lucasfernandes.com.br{path}">
+<meta prefix="og: http://ogp.me/ns#" property="og:title" content="{title}">
+<meta prefix="og: http://ogp.me/ns#" property="og:description" content="I am a fullstack developer, specially for the web, building stylish and responsive layouts and designing well structured APIs and databases.">
+<meta prefix="og: http://ogp.me/ns#" property="og:image" content="https://lucasfernandes.com.br/thumbnail.jpg">
 
 <meta property="twitter:card" content="summary_large_image">
 <meta property="twitter:url" content="https://lucasfernandes.com.br{path}">
