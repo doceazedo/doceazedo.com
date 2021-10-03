fea32665fa5817428c347b3274d0c09c922e1e48
DoceAzedo <oi@lucasfernandes.com.br>
2021-10-03 17:15:35 -0300

    🔍🐛 Fix SEO meta tags

--- src/components/seo.svelte
+++ src/components/seo.svelte
@@ -10,7 +10,7 @@
 <meta property="og:url" content="https://lucasfernandes.com.br{path}">
 <meta property="og:title" content="{title}">
 <meta property="og:description" content="I am a fullstack developer, specially for the web, building stylish and responsive layouts and designing well structured APIs and databases.">
-<meta property="og:image" content="https://lucasfernandes.com.br/thumbnail.jpg">
+<meta property="og:image" name="image" content="https://lucasfernandes.com.br/thumbnail.jpg">
 
 <meta property="twitter:card" content="summary_large_image">
 <meta property="twitter:url" content="https://lucasfernandes.com.br{path}">
