63404d14985afd3e8b496ab2ac27d50a779de860
DoceAzedo <oi@lucasfernandes.com.br>
2021-10-22 12:16:24 -0300

    🐛 Fix now playing to detect "scrobbling now" songs

--- src/routes/api/live.js
+++ src/routes/api/live.js
@@ -8,13 +8,18 @@ export async function get() {
   lastfm = await res.json();
   lastfm = lastfm.recenttracks.track[0];
 
-  let now = new Date();
-  now = new Date(now.setHours(now.getHours() + now.getTimezoneOffset() / 60));
-  const playedAt = new Date(Date.parse(lastfm.date['#text']));
-  const hoursAgo = Math.abs(now - playedAt) / 36e5;
+  let isListening = true;
 
-  // Última música tocada há menos de 15 minutos
-  if (hoursAgo < .25) {
+  if (lastfm.date) {
+    let now = new Date();
+    now = new Date(now.setHours(now.getHours() + now.getTimezoneOffset() / 60));
+    const playedAt = new Date(Date.parse(lastfm.date['#text']));
+    const hoursAgo = Math.abs(now - playedAt) / 36e5;
+    // Última música tocada há menos de 15 minutos
+    isListening = hoursAgo < .25;
+  }
+
+  if (isListening) {
     res = await fetch(`https://api.deezer.com/search?q=${lastfm.artist['#text']} ${lastfm.name}`);
     deezer = await res.json();
     deezer = deezer.data[0].album.cover_medium;
