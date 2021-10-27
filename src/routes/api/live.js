1010617bc34228fbd159b47dfd3a897030256785
DoceAzedo <oi@lucasfernandes.com.br>
2021-10-27 12:18:44 -0300

    🐎 Call Twitch and Spotify APIs concurrently

--- src/routes/api/live.js
+++ src/routes/api/live.js
@@ -2,7 +2,7 @@ import SpotifyWebApi from 'spotify-web-api-node';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export async function get() {
+const fetchNowPlaying = async () => {
   const refreshToken = process.env['SPOTIFY_REFRESH_TOKEN'];
   const clientId = process.env['SPOTIFY_CLIENT_ID'];
   const clientSecret = process.env['SPOTIFY_CLIENT_SECRET'];
@@ -19,19 +19,11 @@ export async function get() {
   spotifyApi.setAccessToken(spotifyTokens.body.access_token);
 
   const currentlyPlaying = await spotifyApi.getMyCurrentPlayingTrack();
-
-  const twitch = await(await fetch(`https://api.twitch.tv/helix/streams?user_login=${process.env['TWITCH_CHANNEL']}`, {
-    headers: {
-      'Authorization': `Bearer ${process.env['TWITCH_OAUTH_TOKEN']}`,
-      'Client-Id': process.env['TWITCH_CLIENT_ID']
-    }
-  })).json();
-
   
   let nowPlaying = null;
   if (currentlyPlaying.body?.item) {
     const item = currentlyPlaying.body.item;
-    
+
     nowPlaying = {
       title: item.name,
       artist: item.artists[0].name,
@@ -39,10 +31,27 @@ export async function get() {
     };
   }
 
+  return nowPlaying;
+}
+
+const fetchIsLive = async () => {
+  const twitch = await(await fetch(`https://api.twitch.tv/helix/streams?user_login=${process.env['TWITCH_CHANNEL']}`, {
+    headers: {
+      'Authorization': `Bearer ${process.env['TWITCH_OAUTH_TOKEN']}`,
+      'Client-Id': process.env['TWITCH_CLIENT_ID']
+    }
+  })).json();
+
+  return twitch.data?.length ? true : false;
+}
+
+export async function get() {
+  const [nowPlaying, isLive] = await Promise.all([fetchNowPlaying(), fetchIsLive()]);
+
   return {
     body: {
       nowPlaying,
-      isLive: twitch.data?.length ? true : false,
+      isLive,
     }
   }
 }
