9fac3849b7a1a84a76f0a9c00642d65c643e8b48
DoceAzedo <oi@lucasfernandes.com.br>
2021-11-02 20:10:28 -0300

    💄 Configure linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'prettier'],
+	extends: ['airbnb-base', 'prettier'],
 	plugins: ['svelte3'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	parserOptions: {
