f25c56ae7d2532669d0bd0c36d18d461022da183
DoceAzedo <oi@lucasfernandes.com.br>
2022-02-17 21:37:31 -0300

    👽 Fix shit SvelteKit broke

--- svelte.config.js
+++ svelte.config.js
@@ -6,8 +6,6 @@ import vercel from '@sveltejs/adapter-vercel';
 const config = {
   preprocess: sveltePreprocess(),
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
     adapter: vercel(),
 	},
 };
