c72eca78d6042fe5c4006c1435f7e5af8e8e0bd9
Lucas Fernandes <me@lucasfernandes.com.br>
2022-05-03 23:51:33 -0300

    🔗 Update topbar link

--- src/lib/modules/topbar/topbar-domain-migration.svelte
+++ src/lib/modules/topbar/topbar-domain-migration.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { Topbar } from '$lib/components';
 
-  const content = `"I'm sorry, the old Lucas can't come to the phone right now. Why? Oh 'cause they're <b>dead</b>!" I've fully embraced my DoceAzedo nickname as a brand that can represent not only me personally but also my work. <a href="/blog/the-doceazedo-brand">Learn more about it.</a>`;
+  const content = `"I'm sorry, the old Lucas can't come to the phone right now. Why? Oh 'cause they're <b>dead</b>!" I've fully embraced my DoceAzedo nickname as a brand that can represent not only me personally but also my work. <a href="/blog/who-is-doceazedo">Learn more about it.</a>`;
 
   export let ref: string;
 </script>
