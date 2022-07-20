4f94b9294cf9aaabd63fc5de1a872df9948f907d
DoceAzedo <me@doceazedo.com>
2022-07-20 12:55:57 -0300

    Fix getPosts

--- src/lib/utils/get-posts.ts
+++ src/lib/utils/get-posts.ts
@@ -1,12 +1,22 @@
 /* eslint-disable no-useless-escape */
 
+type BlogPostFile = {
+  default: Record<string, unknown>;
+  metadata: {
+    title: string;
+    date: string;
+    tags: string[];
+    icon: string;
+  };
+};
+
 export const getPosts = async (qty?: number) => {
-  const files = import.meta.glob('../../routes/blog/*.md');
+  const files = import.meta.glob<BlogPostFile>('../../routes/blog/*.md');
   const posts = Object.entries(files);
   const size = qty || posts.length;
 
   const allPosts = await Promise.all(
-    posts.slice(0, size).map(async ([path, resolver]) => {
+    posts.map(async ([path, resolver]) => {
       const { metadata } = await resolver();
       const slug = path.match(/[^\\\/]+(?=\.[\w]+$)|[^\\\/]+$/)[0];
       const post = {
@@ -18,9 +28,9 @@ export const getPosts = async (qty?: number) => {
     })
   );
 
-  const sortedPosts = allPosts.sort(
-    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-  );
+  const sortedPosts = allPosts
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+    .slice(0, size);
 
   return {
     body: {
