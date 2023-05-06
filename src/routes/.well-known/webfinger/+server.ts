63b38205757e837ccc52a44b70dc9c9783b2a18a
doceazedo <me@doceazedo.com>
2023-05-06 03:57:30 -0300

    Fix Mastodon webfinger headers

--- src/routes/.well-known/webfinger/+server.ts
+++ src/routes/.well-known/webfinger/+server.ts
@@ -8,27 +8,34 @@ export const GET = async ({ url }) => {
   const resource = url.searchParams.get('resource');
   if (resource != `acct:${MASTODON_ALIAS}`) throw error(404);
 
-  return json({
-    subject: `acct:${MASTODON_USERNAME}@${MASTODON_DOMAIN}`,
-    aliases: [
-      `https://${MASTODON_DOMAIN}/@${MASTODON_USERNAME}`,
-      `https://${MASTODON_DOMAIN}/users/${MASTODON_USERNAME}`
-    ],
-    links: [
-      {
-        rel: `http://webfinger.net/rel/profile-page`,
-        type: `text/html`,
-        href: `https://${MASTODON_DOMAIN}/@${MASTODON_USERNAME}`
-      },
-      {
-        rel: `self`,
-        type: `application/activity+json`,
-        href: `https://${MASTODON_DOMAIN}/users/${MASTODON_USERNAME}`
-      },
-      {
-        rel: `http://ostatus.org/schema/1.0/subscribe`,
-        template: `https://${MASTODON_DOMAIN}/authorize_interaction?uri={uri}`
+  return json(
+    {
+      subject: `acct:${MASTODON_USERNAME}@${MASTODON_DOMAIN}`,
+      aliases: [
+        `https://${MASTODON_DOMAIN}/@${MASTODON_USERNAME}`,
+        `https://${MASTODON_DOMAIN}/users/${MASTODON_USERNAME}`
+      ],
+      links: [
+        {
+          rel: `http://webfinger.net/rel/profile-page`,
+          type: `text/html`,
+          href: `https://${MASTODON_DOMAIN}/@${MASTODON_USERNAME}`
+        },
+        {
+          rel: `self`,
+          type: `application/activity+json`,
+          href: `https://${MASTODON_DOMAIN}/users/${MASTODON_USERNAME}`
+        },
+        {
+          rel: `http://ostatus.org/schema/1.0/subscribe`,
+          template: `https://${MASTODON_DOMAIN}/authorize_interaction?uri={uri}`
+        }
+      ]
+    },
+    {
+      headers: {
+        'Content-Type': 'application/jrd+json'
       }
-    ]
-  });
+    }
+  );
 };
