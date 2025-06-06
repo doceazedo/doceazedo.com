980bdf821bf2d121ce571ff2c4f949b6d105d1f0
DoceAzedo <me@doceazedo.com>
2025-06-06 06:42:29 -0300

    setup shadcn/ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { paraglideVitePlugin } from '@inlang/paraglide-js';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		sveltekit(),
 		paraglideVitePlugin({
-			project: './project.inlang',
-			outdir: './src/lib/paraglide'
-		})
-	]
+			project: "./project.inlang",
+			outdir: "./src/lib/paraglide",
+		}),
+	],
 });
