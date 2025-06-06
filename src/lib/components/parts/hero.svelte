9464997d7f5f5bf37cf7b13e18baff08ed40a6e9
DoceAzedo <me@doceazedo.com>
2025-06-06 08:05:37 -0300

    navbar and hero

---
+++ src/lib/components/parts/hero.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { ArrowRightUpLineArrows } from "svelte-remix";
+</script>
+
+<section class="relative flex w-full justify-between py-24">
+	<div class="flex flex-col">
+		<h1 class="max-w-[12ch] text-6xl/18">I’m Doce Fernandes</h1>
+		<a
+			href="/"
+			class="border-b-primary text-primary hover:text-foreground hover:border-b-foreground group mt-11 mr-24 ml-auto flex w-fit gap-1.5 border-b pb-1 leading-5 font-medium transition-all"
+		>
+			<span>Let's work<br />together</span>
+			<ArrowRightUpLineArrows
+				class="size-5 transition-all group-hover:translate-x-0.5 group-hover:-translate-y-0.5"
+			/>
+		</a>
+	</div>
+	<figure class="group relative aspect-[4/5] w-60">
+		<img
+			src="/img/selfie.webp"
+			alt=""
+			class="relative z-20 rounded object-cover transition-all group-hover:-translate-x-3 group-hover:translate-y-3"
+		/>
+		<span
+			class="border-foreground/10 absolute top-0 left-0 z-30 size-full rounded border transition-all group-hover:-translate-x-3 group-hover:translate-y-3"
+		></span>
+		<span
+			class="border-primary absolute top-11 -left-11 z-10 size-full rounded border"
+		></span>
+		<span
+			class="border-primary/20 absolute top-[88px] left-[-88px] z-10 size-full rounded border transition-all group-hover:top-[76px] group-hover:left-[-76px]"
+		></span>
+	</figure>
+	<p class="text-foreground/70 mt-auto max-w-[27ch] translate-y-6">
+		A <span class="text-foreground">🦄</span> developer, designer, and DJ. I
+		build tools with Rust & Svelte, maintain FOSS projects, work at
+		<a
+			href="/"
+			class="text-foreground hover:text-primary underline transition-all"
+			>RailEurope</a
+		>, and spin DJ sets on weekends
+		<span class="text-foreground">✌️</span>
+	</p>
+</section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,51 @@
 <script lang="ts">
 	import "../app.css";
+	import BrazilFlag from "$lib/components/icons/brazil-flag.svg?component";
+	import { ArrowDownSLineArrows } from "svelte-remix";
 
 	let { children } = $props();
+
+	const PAGES = [
+		{ label: "Speaking", href: "/" },
+		{ label: "Blog", href: "/" },
+		{ label: "Resources", href: "/" },
+		{ label: "Stack", href: "/" },
+		{ label: "About me", href: "/" },
+	];
 </script>
 
-{@render children()}
+<nav class="fixed top-0 left-0 h-20 w-full">
+	<div class="mx-auto flex size-full max-w-5xl items-center">
+		<a href="/" class="flex items-center gap-1.5 text-2xl">
+			<img src="/img/memoji.png" alt="" class="size-8" />
+			<span>
+				doce<span class="text-foreground/70">.sh</span>
+			</span>
+		</a>
+
+		<div class="text-foreground/70 -mr-3 ml-auto flex h-full items-center">
+			{#each PAGES as page}
+				<a
+					href={page.href}
+					class="hover:text-foreground flex h-full items-center px-3 transition-all"
+				>
+					{page.label}
+				</a>
+			{/each}
+
+			<button
+				class="hover:text-foreground flex h-full cursor-pointer items-center gap-3 px-3 transition-all"
+			>
+				<BrazilFlag class="size-6" />
+				<span class="flex items-center gap-1.5">
+					Português
+					<ArrowDownSLineArrows class="size-4" />
+				</span>
+			</button>
+		</div>
+	</div>
+</nav>
+
+<div class="mx-auto max-w-5xl pt-20">
+	{@render children()}
+</div>
