6dc53b2eb0c66db3542017fee8ddfe10b6b7482c
DoceAzedo <me@doceazedo.com>
2025-06-12 14:16:55 -0300

    convert memoji to mov so it finally plays on ios

--- src/lib/components/memoji.svelte
+++ src/lib/components/memoji.svelte
@@ -6,19 +6,22 @@
 
 	let video = $state<HTMLVideoElement>();
 
+	let isSafari = $state(false);
+
 	onMount(() => {
-		video?.play();
+		isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 	});
 </script>
 
 <!-- svelte-ignore a11y_mouse_events_have_key_events -->
 <video
 	bind:this={video}
+	src="/video/memoji.{isSafari ? 'mov' : 'webm'}"
 	class={cn("-ml-1.5 size-10", className)}
 	controls={false}
 	muted
+	autoplay
 	playsinline
 	onmouseover={() => video?.play()}
 >
-	<source src="/video/memoji.webm" type="video/webm" />
 </video>
