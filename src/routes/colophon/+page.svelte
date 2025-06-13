be57222cb322c4308b6ea9c8ee1c3ac24afe5ffa
DoceAzedo <me@doceazedo.com>
2025-06-13 15:51:52 -0300

    add /colophon (wip)

---
+++ src/routes/colophon/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { m } from "$lib/paraglide/messages";
+</script>
+
+<hgroup class="mb-3 py-3 md:mb-6 md:py-6">
+	<h1 class="text-3xl md:text-4xl">{m.colophon()}</h1>
+	<p
+		class="text-body [&>a]:text-foreground [&>a]:hover:text-primary [&>a]:underline [&>a]:transition-all"
+	>
+		How this site is made.
+	</p>
+</hgroup>
