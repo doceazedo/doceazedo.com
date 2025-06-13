40d680073b5c9e775d3c953a51909f22c98f05b6
DoceAzedo <me@doceazedo.com>
2025-06-13 11:22:37 -0300

    add reading section to /now

--- src/routes/now/+page.svelte
+++ src/routes/now/+page.svelte
@@ -6,13 +6,38 @@
 	import { toFixedIfNecessary } from "$lib/utils/numbers";
 	import {
 		ArrowRightLineArrows,
+		BookMarkedFillDocument,
+		CalendarCheckLineBusiness,
 		CalendarLineBusiness,
+		GamepadFillDevice,
 		GamepadLineDevice,
 		HeadphoneFillMedia,
+		UserLineUserFaces,
 	} from "svelte-remix";
 	import NowSectionTitle from "./now-section-title.svelte";
 	import { Button } from "$lib/components/ui/button";
 	import { LAST_PLAYED_TRACKS } from "$lib/stores";
+	import { Progress } from "$lib/components/ui/progress";
+
+	const BOOK = {
+		title: {
+			en: "The Name of the Wind",
+			pt: "O Nome do Vento",
+		},
+		subtitle: {
+			en: "The Kingkiller Chronicle: Day One",
+			pt: "A Crônica do Matador do Rei: Primeiro Dia",
+		},
+		author: "Patrick Rothfuss",
+		cover: "/img/o-nome-do-vento.webp",
+		url: "https://www.goodreads.com/book/show/8480575-o-nome-do-vento",
+		pages: {
+			read: 0,
+			total: 656,
+			updatedAt: new Date("2025/06/13 GMT-3"),
+		},
+		startedAt: new Date("2025/06/13 GMT-3"),
+	};
 
 	const getLastPlayedGames = async () => {
 		try {
@@ -29,13 +54,90 @@
 		if (img.src === placeholder) return;
 		img.src = placeholder;
 	};
+
+	let innerWidth = $state(0);
+	let isDesktop = $derived(innerWidth >= 768);
 </script>
+
+<svelte:window bind:innerWidth />
 
 <div class="flex flex-col gap-12">
 	<hgroup class="py-3 md:py-6">
 		<h1 class="text-3xl md:text-4xl">{m.now()}</h1>
 		<p class="text-body">{m.now_subtitle()}</p>
 	</hgroup>
+
+	<NowSectionTitle
+		icon={BookMarkedFillDocument}
+		title={m.reading()}
+		subtitle={m.reading_subtitle()}
+	/>
+	<a
+		href={BOOK.url}
+		target="_blank"
+		rel="noopener noreferrer"
+		class="ease-elastic hover:bg-muted flex grid-cols-6 gap-6 rounded border p-3 transition-all hover:scale-105 md:grid"
+	>
+		<figure
+			class="relative h-fit w-1/4 shrink-0 before:absolute before:top-0 before:left-0 before:size-full before:rounded before:border before:border-white/20 md:w-auto"
+		>
+			<img src={BOOK.cover} alt="" class="w-full rounded" />
+		</figure>
+		<div class="col-span-5 flex w-full flex-col gap-1.5 md:gap-3">
+			<hgroup>
+				<h3 class="-mb-0.5 text-xl md:-mb-px md:text-2xl">
+					{BOOK.title[getLocale()]}
+				</h3>
+				<p class="text-body text-xs leading-4 md:text-sm">
+					{BOOK.subtitle[getLocale()]}
+				</p>
+			</hgroup>
+			<div
+				class="text-body flex flex-col gap-1.5 text-sm md:flex-row md:gap-3 md:text-base"
+			>
+				<p class="hidden items-center gap-1 md:flex">
+					<UserLineUserFaces class="text-foreground size-3.5 md:size-4.5" />
+					{BOOK.author}
+				</p>
+				<span class="hidden opacity-80 md:block">&bull;</span>
+				<div class="flex items-center gap-1">
+					<CalendarLineBusiness class="text-foreground size-3.5 md:size-4.5" />
+					<p class="[&>span]:text-foreground">
+						{@html m.started_at({
+							date: BOOK.startedAt.toLocaleDateString(getLocale()),
+						})}
+					</p>
+				</div>
+			</div>
+			<div class="mt-auto flex flex-col gap-1.5">
+				<div class="flex items-center justify-between text-sm md:text-base">
+					<p class="text-body [&>span]:text-foreground">
+						{@html m.pages_read({
+							current: BOOK.pages.read,
+							total: BOOK.pages.total,
+						})}
+					</p>
+					<div class="text-body flex items-center gap-1 text-sm md:text-base">
+						<CalendarCheckLineBusiness
+							class="text-foreground size-3.5 md:size-4.5"
+						/>
+						<p class="[&>span]:text-foreground">
+							{#if isDesktop}
+								{@html m.updated_at({
+									date: BOOK.pages.updatedAt.toLocaleDateString(getLocale()),
+								})}
+							{:else}
+								{BOOK.pages.updatedAt.toLocaleDateString(getLocale())}
+							{/if}
+						</p>
+					</div>
+				</div>
+				<Progress value={BOOK.pages.read} max={BOOK.pages.total} />
+			</div>
+		</div>
+	</a>
+
+	<hr />
 
 	<NowSectionTitle
 		icon={HeadphoneFillMedia}
@@ -138,7 +240,7 @@
 	<hr />
 
 	<NowSectionTitle
-		icon={GamepadLineDevice}
+		icon={GamepadFillDevice}
 		title={m.playing_games()}
 		subtitle={m.playing_games_subtitle()}
 	/>
@@ -177,7 +279,7 @@
 							</p>
 							<div class="flex items-center justify-center gap-1">
 								{#if game.updatedAt}
-									<CalendarLineBusiness class="text-body size-4" />
+									<CalendarCheckLineBusiness class="text-body size-4" />
 								{:else}
 									<GamepadLineDevice class="text-body size-4" />
 								{/if}
