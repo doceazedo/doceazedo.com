2cf836f3adf61afb92e6aacc5af6301e840e21f2
DoceAzedo <me@doceazedo.com>
2025-06-16 16:12:26 -0300

    remove debug code

--- src/routes/me/+page.server.ts
+++ src/routes/me/+page.server.ts
@@ -1,10 +1,8 @@
-import { DEBUG_LOCATION } from "$lib/constants.js";
-
 export const load = async ({ request }) => {
 	const lon = request.headers.get("x-vercel-ip-longitude");
 	const lat = request.headers.get("x-vercel-ip-latitude");
 	const location: [number, number] | null =
-		lon && lat ? [parseFloat(lon), parseFloat(lat)] : DEBUG_LOCATION;
+		lon && lat ? [parseFloat(lon), parseFloat(lat)] : null;
 	return {
 		location,
 	};
