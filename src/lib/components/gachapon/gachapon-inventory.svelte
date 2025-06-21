b6ab8f8a736662e22c1feef8a4ac46c7fe2d58e8
DoceAzedo <me@doceazedo.com>
2025-06-21 19:55:00 -0300

    inventory items counter

--- src/lib/components/gachapon/gachapon-inventory.svelte
+++ src/lib/components/gachapon/gachapon-inventory.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { CopperCoinLineFinance } from "svelte-remix";
+	import { CopperCoinLineFinance, Progress5LineSystem } from "svelte-remix";
 	import { ITEMS, RARITIES } from "./constants";
 	import { cn } from "$lib/utils";
 	import { GAME_DATA, TWEENED_BALANCE } from "./stores";
@@ -24,14 +24,32 @@
 			</div>
 			<div class="-mt-22 mb-6 w-full rounded border pt-22">
 				<div class="mx-auto grid w-2/3 grid-cols-3 gap-x-3 gap-y-6 py-6">
-					<div class="col-span-3 flex items-center justify-center gap-1.5">
-						<CopperCoinLineFinance class="size-5 text-amber-500" />
-						{Math.floor(TWEENED_BALANCE.current).toLocaleString(getLocale())}
+					<div class="col-span-3 flex items-center justify-center gap-6">
+						<div class="flex items-center gap-1.5">
+							<CopperCoinLineFinance class="size-5 text-amber-500" />
+							{Math.floor(TWEENED_BALANCE.current).toLocaleString(getLocale())}
+						</div>
+						<div class="flex items-center gap-1.5">
+							<Progress5LineSystem class="text-body size-5" />
+							<p>
+								{$GAME_DATA.inventory.length}
+								<span class="text-body">/</span>
+								{ITEMS.length}
+							</p>
+						</div>
 					</div>
 					{#each RARITIES as rarity}
 						<div class="flex flex-col items-center gap-px">
 							<rarity.icon class={cn("size-5", rarity.textColor)} />
-							<p>0</p>
+							<p>
+								{$GAME_DATA.inventory
+									.filter(
+										(item) =>
+											ITEMS.find((itemData) => itemData.id === item.item)
+												?.rarity === rarity.id,
+									)
+									.reduce((total, entry) => total + entry.qty, 0)}
+							</p>
 						</div>
 					{/each}
 				</div>
