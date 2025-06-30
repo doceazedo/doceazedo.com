ce0c3ee50c2f5d6d638960a4ea02a536e611a07f
DoceAzedo <me@doceazedo.com>
2025-06-30 14:12:01 -0300

    fix paraglide strategy (i could swear i had fixed that already)

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
 		paraglideVitePlugin({
 			project: "./project.inlang",
 			outdir: "./src/lib/paraglide",
+			strategy: ["cookie", "preferredLanguage", "baseLocale"],
 		}),
 		arraybuffer(),
 	],
